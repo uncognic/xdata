fdbf086d3603f71a6630023c195a46919728fe83
uncognic <uncognic@proton.me>
2025-12-07 19:10:11 -0500

    moved it to its own bin

--- src/bin/mkfs.rs
+++ src/bin/mkfs.rs
@@ -1,15 +1,17 @@
-
 use std::fs::File;
 use std::io::{Write, Seek, SeekFrom};
-fn main() {
-    let mut file = File::create("disk.img").expect("failed to create img");
-    let magic: u64 = 0x54414458;       // "TADX" but becomes "XDAT" because of endianness
+
+fn mkfs(filename: &str) {
+    let mut file = File::create(filename).expect("failed to create img");
+
+    let magic: u64 = 0x54414458; // "TADX" -> XDAT
     let version: u32 = 1;
     let blktot: u64 = 1;
     let metastart: u64 = 1;
     let metact: u64 = 0;
     let uuid: [u8; 16] = [0u8; 16];
     let sha512: [u8; 64] = [0u8; 64];
+
     let mut buf = [0u8; 4096];
     buf[0..8].copy_from_slice(&magic.to_le_bytes());
     buf[8..12].copy_from_slice(&version.to_le_bytes());
@@ -21,4 +23,12 @@ fn main() {
 
     file.seek(SeekFrom::Start(0)).unwrap();
     file.write_all(&buf).unwrap();
+}
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        mkfs("disk.img");
+        std::process::exit(1);
+    }
+    mkfs(&args[1]);
 }
